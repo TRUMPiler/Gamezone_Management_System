99268baac7e93cca4c142bace7a779b3d1aee852
NAISHAL doshi <cjesus69133@gmail.com>
2025-01-12 18:29:27 +0530

    GG

---
+++ GameZoneManagementSystem/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameZoneManagementSystem.Models;
+namespace GameZoneManagementSystem.Controllers
+{
+    
+    public class CustomerController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult register()
+        {
+            return View();
+        }
+    }
+}
