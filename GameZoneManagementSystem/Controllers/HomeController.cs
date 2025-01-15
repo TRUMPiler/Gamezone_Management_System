70f5ff196da2934c51bd37b2f459d300a816a278
NAISHAL doshi <cjesus69133@gmail.com>
2025-01-15 22:53:49 +0530

    New

--- GameZoneManagementSystem/Controllers/HomeController.cs
+++ GameZoneManagementSystem/Controllers/HomeController.cs
@@ -2,13 +2,14 @@ using System.Data.SqlClient;
 using System.Diagnostics;
 using GameZoneManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Session;
-
+using Microsoft.AspNetCore.Http;
 namespace GameZoneManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
-        
+        string SessionName = "Login";
+        string SessionEmail = "Email";
+        string SessionRole = "Role";
         string script="";
         string con = "Data Source=NAISHALTUF;Initial Catalog=GZMS;Integrated Security=True;";
         private readonly ILogger<HomeController> _logger;
@@ -20,21 +21,39 @@ namespace GameZoneManagementSystem.Controllers
         
         public bool isLoggedIn()
         {
-          
-            //if(HttpContext.Session.GetInt32("Login")>0&&(string.IsNullOrEmpty(HttpContext.Session.GetString("Role"))))
+
+            //if (HttpContext.Session == null)
             //{
+            //    Console.WriteLine("Session object is null");
             //    return true;
             //}
-            //else
-            //{
-            //    HttpContext.Session.SetInt32("Login", 0);
 
+            //if (!HttpContext.Session.Keys.Contains(SessionName))
+            //{
+            //    Console.WriteLine($"Session key '{SessionName}' does not exist");
+            //    return true;
+            //}
+
+            //int? sessionValue = HttpContext.Session.GetInt32(SessionName);
+            //if (!sessionValue.HasValue)
+            //{
+            //    Console.WriteLine("Session value is null");
+            //    return true;
             //}
             return false;
+
         }
         public IActionResult Index()
         {
-            return View();
+            if(HttpContext.Session.GetInt32(SessionName)!=null)
+            {
+                return RedirectToAction("Index","Customer");
+            }
+            else
+            {
+                return View();
+            }
+            
         }
         public String ReturnView(string role)
         {
@@ -101,18 +120,18 @@ namespace GameZoneManagementSystem.Controllers
             HashPasswordController hp = new HashPasswordController();
             if (!string.IsNullOrEmpty(storedHashedPassword) && hp.VerifyPassword(user.Password, storedHashedPassword))
             {
-                HttpContext.Session.SetInt32("Login", 1);
-                HttpContext.Session.SetString("Email", user.Email.ToString());
-                HttpContext.Session.SetString("Role", user.Role.ToString());
+                HttpContext.Session.SetInt32(SessionName, 1);
+                HttpContext.Session.SetString(SessionEmail, user.Email.ToString());
+                HttpContext.Session.SetString(SessionRole, user.Role.ToString());
 
-                if (user.Role=="c")
-                {
+                //if (user.Role=="c")
+                //{
                     
-                    user.Role = "customer";
-                    script = $"<script>alert('Login successful! Welcome {user.Role}');window.location='/Customer/Index';</script>";
-                    return Content(script, "text/html");
-                }
-                script = $"<script>alert('Login successful! Welcome {user.Role}');window.location='/Customer/Index';</script>";
+                //    user.Role = "customer";
+                //    script = $"<script>alert('Login successful! Welcome {user.Role}');window.location='/Customer/Index';</script>";
+                //    return Content(script, "text/html");
+                //}
+                script = $"<script>alert('Login successful! Welcome {user.Role}');window.location='/Home/Index';</script>";
                 return Content(script, "text/html");
             }
             else
--- GameZoneManagementSystem/Program.cs
+++ GameZoneManagementSystem/Program.cs
@@ -5,9 +5,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDistributedMemoryCache(); // Required for session storage
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
-    options.Cookie.HttpOnly = true;                // Make the cookie inaccessible via client-side scripts
-    options.Cookie.IsEssential = true;            // Mark the session cookie as essential
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
 });
 
 var app = builder.Build();
@@ -26,7 +26,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
